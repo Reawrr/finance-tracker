438ef6fe4d13fcda101dd003b2c3582e480dd52a
VarheaPutraAmrillah <rea.amrillah@gmail.com>
2026-06-25 02:04:47 +0700

    app jees

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://your-app.vercel.app'],
+  origin: ['http://localhost:5173', 'https://finance-tracker.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
